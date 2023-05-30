fe648668c53e92d3147e9a3ac138e0ab93b9ffa6
Paulo Matos <pmatos@linki.tools>
2023-05-30 14:58:30 +0200

    Cache image information

--- src/similarity.cpp
+++ src/similarity.cpp
@@ -1,13 +1,33 @@
 #include <bitset>
+#include <chrono>
 #include <cstdint>
 #include <filesystem>
 #include <iostream>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/opencv.hpp>
 #include <set>
 #include <string>
 #include <vector>
 
 namespace fs = std::filesystem;
+
+class Timer {
+public:
+  Timer(const std::string &name)
+      : m_Name(name), m_Start(std::chrono::high_resolution_clock::now()) {}
+
+  ~Timer() {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - m_Start;
+    std::cout << "Block " << m_Name << " took: " << elapsed.count() << "ms\n";
+  }
+
+private:
+  std::string m_Name;
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+};
+
+#define TIME_BLOCK(name) Timer timer##__LINE__(name)
 
 uint64_t computeDHash(const cv::Mat &img) {
   cv::Mat resizedImg, grayImg;
@@ -25,19 +45,42 @@ uint64_t computeDHash(const cv::Mat &img) {
   return hash;
 }
 
+class Image {
+public:
+  Image(const std::string &Path) : Path(Path) {
+    ImgData = cv::imread(Path);
+    if (ImgData.empty()) {
+      std::cerr << "Warning: Could not load image " << Path << "\n";
+      Hash = 0;
+    } else
+      Hash = computeDHash(ImgData);
+  }
+
+  ~Image() {
+    ImgData.release();
+    Path.clear();
+  }
+
+  inline uint64_t getHash() const { return Hash; }
+  inline const std::string &getPath() const { return Path; }
+  inline bool empty() const { return ImgData.empty(); }
+
+private:
+  cv::Mat ImgData;
+  std::string Path;
+  uint64_t Hash;
+};
+
 int hammingDistance(uint64_t hash1, uint64_t hash2) {
   return std::bitset<64>(hash1 ^ hash2).count();
 }
 
-double compareImagesHashed(const cv::Mat &img1, const cv::Mat &img2) {
+double compareImagesHashed(const Image &img1, const Image &img2) {
   if (img1.empty() || img2.empty()) {
-    std::cerr << "Error: Empty input image(s)" << std::endl;
     return 0.0;
   }
 
-  uint64_t hash1 = computeDHash(img1);
-  uint64_t hash2 = computeDHash(img2);
-  int distance = hammingDistance(hash1, hash2);
+  int distance = hammingDistance(img1.getHash(), img2.getHash());
 
   // Normalize the Hamming distance to a similarity score in the range [0, 1].
   return 1.0 - static_cast<double>(distance) / 64.0;
@@ -62,51 +105,44 @@ std::vector<std::vector<std::string>> getSimilaritySets(const std::string &path,
   std::vector<std::vector<std::string>> similaritySets;
 
   // List all image files under the given path.
-  std::vector<std::string> imageFiles;
+  std::vector<Image> imageFiles;
   for (const auto &entry : fs::recursive_directory_iterator(
            path, fs::directory_options::skip_permission_denied)) {
     if (entry.is_regular_file() && !fs::is_symlink(entry) &&
         isImageFile(entry.path().string())) {
-      imageFiles.push_back(entry.path().string());
+      if (imageFiles.size() % 100 == 0)
+        std::cout << "Loaded " << imageFiles.size() << " image files.\n";
+      imageFiles.emplace_back(entry.path().string());
     }
   }
   std::cout << "Found " << imageFiles.size() << " image files.\n";
 
   // Compare every pair of images.
-  for (const auto &imgPath1 : imageFiles) {
-    cv::Mat img1 = cv::imread(imgPath1);
+  for (const auto &img1 : imageFiles) {
     bool addedToSet = false;
 
-    std::cout << ".";
-    std::cout.flush();
     // Instead of comparing imgPath1 with all images in the set, we
     // compare it with just a random image in the set.
     for (auto &similaritySet : similaritySets) {
-      std::cout << "@";
-      std::cout.flush();
-
       size_t sz = similaritySet.size();
       size_t randIndex = std::rand() % sz;
       auto it = similaritySet.begin();
       std::advance(it, randIndex);
-      const auto &imgPath2 = *it;
-
-      cv::Mat img2 = cv::imread(imgPath2);
+      const auto &img2 = *it;
       double similarity = compareImagesHashed(img1, img2);
 
       if (similarity >= threshold) {
-        similaritySet.emplace_back(imgPath1);
+        similaritySet.emplace_back(img1.getPath());
         addedToSet = true;
         break;
       }
     }
 
-    // If the image was not added to any existing set, create a new set for it.
+    // If the image was not added to any existing set, create a new set for
+    // it.
     if (!addedToSet) {
-      std::cout << "!";
-      std::cout.flush();
       std::vector<std::string> newSet;
-      newSet.emplace_back(imgPath1);
+      newSet.push_back(img1.getPath());
       similaritySets.emplace_back(newSet);
     }
   }
@@ -132,8 +168,8 @@ int main(int argc, char *argv[]) {
       return 1;
     }
 
-    cv::Mat img1 = cv::imread(argv[2]);
-    cv::Mat img2 = cv::imread(argv[3]);
+    Image img1(argv[2]);
+    Image img2(argv[3]);
     double similarityHashed = compareImagesHashed(img1, img2);
     std::cout << "Similarity (hashed): " << similarityHashed << std::endl;
   } else {
